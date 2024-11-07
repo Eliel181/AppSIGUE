74f4fdf56dcf21166502508e6b3e9a6db72638a9
Leo-2301 <leonelgero15@gmail.com>
2024-11-07 18:10:13 -0300

    Agregando modulo de LOGIN

---
+++ Entities/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string LoginName { get; set; }
+        public string Password { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Rol { get; set; }
+        public byte[] Foto { get; set; }
+
+    }
+}
---
+++ Entities/UsuarioCache.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public static class UsuarioCache
+    {
+        public static int IdUsuario { get; set; }
+        public static string LoginName { get; set; }
+        public static string Password { get; set; }
+        public static string Nombre { get; set; }
+        public static string Apellido { get; set; }
+        public static string Rol { get; set; }
+        public static byte[] Foto { get; set; }
+    }
+}
